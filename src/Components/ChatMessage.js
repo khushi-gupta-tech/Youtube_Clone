931773d4166149ac1eaa13acdaeb3783b5b1b69b
khushi gupta <kg834208@gmail.com>
2025-09-01 19:04:02 +0530

    Add ChatMessage component

---
+++ src/Components/ChatMessage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const ChatMessage = ({name,message}) => {
+  return (
+    <div className='flex items-center shadow-sm p-2'>
+        <img
+          className="h-8 cursor-pointer"
+          src="https://tse2.mm.bing.net/th/id/OIP.fqSvfYQB0rQ-6EG_oqvonQHaHa?pid=Api&P=0&h=180"
+          alt="user"
+        />
+        <span className='font-bold px-2'>{name}</span>
+        <span>{message}</span>
+        
+    </div>
+  )
+}
+
+export default ChatMessage
