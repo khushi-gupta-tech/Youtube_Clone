75a5a428d903299529cf3ea3738dcbca17dce84e
khushi gupta <kg834208@gmail.com>
2025-09-01 19:00:14 +0530

    all constants

---
+++ src/utils/constants.js
@@ -0,0 +1,12 @@
+const GOOGLE_API_KEY = "AIzaSyBGBbCaqdbXOE5gnboh2yukrtIV7tr0HiA";
+
+export const YOUTUBE__VIDEO_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=" +
+  GOOGLE_API_KEY;
+
+export const YOUTUBE_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+
+export const OFFSET_LIVE_CHAT = 20;
+
+export const VIDEO_DETAILS_API = (id) =>
+  `https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics,contentDetails&id=${id}&key=${GOOGLE_API_KEY}`;
